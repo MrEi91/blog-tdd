5eee8c91ef28b6d70da44d1054edfc0fe5fbce5e
Eri Irawan <email": null>
2017-03-13 12:13:42 +0700

    add server

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+let index = require('../routes/index')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+app.use('/api', index)
+
+app.listen(3000)
+
+module.exports = app
